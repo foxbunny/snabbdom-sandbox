70ea8cf6ebbba7c076a25742b5b0ee72728b9387
Hajime Yamasaki Vukelic <hayavuk@gmail.com>
2017-03-15 21:45:29 +0100

    Move app to separate locaton

---
+++ src/hello.ts
@@ -0,0 +1,21 @@
+import h from 'snabbdom/h'
+import { VNode } from 'snabbdom/vnode'
+import always = require('ramda/src/always')
+
+export const view = (data: {name: string}): VNode =>
+  h('p', [
+    h('p', 'Hello, ' + data.name),
+    h('p', [
+      h('input.name', {props: {value: data.name}})
+    ])
+  ])
+
+export const updates = (input) =>
+  input.on('input', '.name')
+    .map(e => e.target.value)
+    .map(name => data => ({...data, name: name}))
+
+export const program = (input) => ({
+  updates: updates(input),
+  view: input.createView(view)
+})
--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,6 @@ import always = require('ramda/src/always')
 import xs from 'xstream'
 
 import { start } from './starter'
-
-const mainView = (data: {name: string}): VNode =>
-  h('p', [
-    h('p', 'Hello, ' + data.name),
-    h('p', [
-      h('input.name', {props: {value: data.name}})
-    ])
-  ])
-
-const program = (input) => {
-  const input$ = input.on('input', '.name')
-    .map(e => e.target.value)
-    .map(name => data => ({...data, name: name}))
-
-  return {
-    updates: input$,
-    view: input.createView(mainView)
-  }
-}
+import { program } from './hello'
 
 start(program, '#app', {name: 'World'})
